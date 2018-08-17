66fc63384b921d3084f153213127fcf960a92b62
satonmp <1613181s@stu.kobe-u.ac.jp>
2018-08-17 19:07:02 +0900

    entities.kt書いた

--- src/main/kotlin/com/kyotob/api/model/entities.kt
+++ src/main/kotlin/com/kyotob/api/model/entities.kt
@@ -1,2 +1,31 @@
 package com.kyotob.api.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import java.sql.Timestamp
+
+
+data class Room(
+    var room_id:   Long,
+    var room_name: String
+)
+
+data class user(
+        var user_id:          Long,
+        var user_name:        String,
+        var user_screen_name: String,
+        var password:         String
+)
+
+data class pair(
+        var room_id:   Long,
+        var user_id_1: Long,
+        var user_id_2: Long
+)
+
+data class message(
+        var message_id: Long,
+        var sender_id:  Long,
+        var room_id:    Long,
+        var message:  String,
+        @get:JsonProperty("created_at") var created_at: Timestamp
+)
